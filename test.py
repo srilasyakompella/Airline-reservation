7461fc7e2a08c6764d2a149cac297ad470bb1833
Sai Preeti <psaipreeti@gmail.com>
2022-02-02 12:44:49 +0530

    inserting values in payment table

--- test.py
+++ test.py
@@ -16,16 +16,13 @@ mycursor = mydb.cursor()
 
 mycursor.execute("SELECT * FROM flight  ORDER BY CHARGES")
 myresult = mycursor.fetchall()
-
+pid=0
+sno=0
 l=[]
 for x in myresult:
     l.append(x)
 
-@app.route('/Page4',methods=['POST'])
-def Page4():
-    id= request.form['abc']
-    print(id)
-    return render_template('Page4.html')
+
   
 @app.route('/flights',methods=['POST'])
 def flights():
@@ -39,18 +36,21 @@ def flights():
     age=request.form['age']
     passengers=int(request.form['passengers'])
     name=firstName+lastName
-    # print(fromCity)
-    # print(toCity)
-    # print(departureDate)
-    # print(passengers)
-    # print(firstName)
-    # print(lastName)
-    # print(email)
-    # print(phoneNo)
-    # print(age)
+    print(fromCity)
+    print(toCity)
+    print(departureDate)
+    print(passengers)
+    print(firstName)
+    print(lastName)
+    print(email)
+    print(phoneNo)
+    print(age)
     li=[x for x in range(1,700)]
     passengerId=random.choice(li)
-    mycursor.execute('insert into passengerDetails values(%s,%s,%s,%s,%s)',(passengerId,name,age,email,phoneNo) )
+    global pid 
+    pid=passengerId
+    print('Passenger id in flights is :',pid)
+    mycursor.execute('insert into passengerDetails values(%s,%s,%s,%s,%s)',(pid,name,age,email,phoneNo) )
     mydb.commit()
     # mycursor.execute("SELECT * FROM passengerDetails where passengerId=passId ")
     # result = mycursor.fetchall()
@@ -66,12 +66,34 @@ def flights():
         p=(request.form['phno'+str(i)])
         a=(request.form['age'+str(i)])
         s=fn+ln
-        mycursor.execute('insert into passengerDetails values(%s,%s,%s,%s,%s)',(passengerId+1,s,a,e,p))
+        mycursor.execute('insert into passengerDetails values(%s,%s,%s,%s,%s)',(passengerId,s,a,e,p))
         mydb.commit()
-        
+       
    
     return render_template('home.html',l=l,fromCity=fromCity,toCity=toCity)
-
+@app.route('/Page4',methods=['POST'])
+def Page4():
+    id= request.form['abc']
+    global sno
+    sno= id
+    # print(id)
+    return render_template('Page4.html')
+@app.route('/Page5',methods=['POST','GET'])
+def Page5():
+    if request.method=='POST':
+        cardNumber=request.form['cardNumber']
+        cardOwnerName=request.form['cardOwnerName']
+        print(cardNumber)
+        print(cardOwnerName)
+        li=[x for x in range(1,9000)]
+        paymentId=random.choice(li)
+        print('Passenger id in page5 is :',pid)
+        print(pid)
+        mycursor.execute('insert into payment values(%s,%s,%s,%s,%s)',(pid,sno,cardOwnerName,cardNumber,paymentId) )
+        mydb.commit()
+        return render_template('Page5.html')
+    else:
+        return render_template('Page4.html')
 @app.route('/Page2',methods=['POST','GET'])
 def Page2():
     if request.method=='POST':
@@ -88,7 +110,8 @@ def Page2():
 @app.route('/',methods=['GET'])
 def home():
     return render_template('Page1.html')
-    
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
